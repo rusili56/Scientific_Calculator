3b6d2b4314ccfcc8d34f0a53204582b979864fa3
Rusi Li <rusili56@gmail.com>
2016-10-06 15:14:52 -0400

    Horizontal layout done

---
+++ app/src/main/java/nyc/c4q/rusili/scientific_calculator/Calculator_Horizontal.java
@@ -0,0 +1,49 @@
+package nyc.c4q.rusili.scientific_calculator;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
+
+/**
+ * Created by rusili on 10/5/16.
+ */
+
+public class Calculator_Horizontal extends AppCompatActivity {
+    boolean isDeg = true;
+    TextView tvDeg;
+    TextView tvRad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_horizontal);
+        tvDeg = (TextView) findViewById(R.id.idDeg);
+        tvRad = (TextView) findViewById(R.id.idRad);
+
+    }
+
+    public void onSwitch(View view) {
+        if (isDeg){
+            tvDeg.setText("DEG");
+            tvDeg.setTextColor(Color.parseColor("#f7f7f7"));
+            tvDeg.setBackgroundColor(Color.parseColor("#767676"));
+            tvRad.setText(":::::");
+            tvRad.setTextColor(Color.parseColor("#767676"));
+            tvRad.setBackgroundColor(Color.parseColor("#f7f7f7"));
+            isDeg = false;
+        } else {
+            tvRad.setText("RAD");
+            tvRad.setTextColor(Color.parseColor("#f7f7f7"));
+            tvRad.setBackgroundColor(Color.parseColor("#767676"));
+            tvDeg.setText(":::::");
+            tvDeg.setTextColor(Color.parseColor("#767676"));
+            tvDeg.setBackgroundColor(Color.parseColor("#f7f7f7"));
+            isDeg = true;
+        }
+
+
+    }
+}
--- app/src/main/java/nyc/c4q/rusili/scientific_calculator/Calculator_Vertical.java
+++ app/src/main/java/nyc/c4q/rusili/scientific_calculator/Calculator_Vertical.java
@@ -1,11 +1,14 @@
 package nyc.c4q.rusili.scientific_calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.TextView;
+
+import static android.R.attr.screenOrientation;
 
 public class Calculator_Vertical extends AppCompatActivity {
 
