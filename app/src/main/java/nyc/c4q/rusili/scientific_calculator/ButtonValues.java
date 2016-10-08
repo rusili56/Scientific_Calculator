a2e395d1ad140bbf355dff186ffec1bb0f2e3bae
BlackJack <RedDragon@BlackJacks-MBP.home>
2016-10-08 02:26:17 -0400

    added Keypad class and enum Button Vals / changed xml button name convention sin = btn_sin

---
+++ app/src/main/java/nyc/c4q/rusili/scientific_calculator/ButtonValues.java
@@ -0,0 +1,8 @@
+package nyc.c4q.rusili.scientific_calculator;
+
+/**
+ * Created by RedDragon on 10/8/16.
+ */
+
+public enum ButtonValues {
+}
---
+++ app/src/main/java/nyc/c4q/rusili/scientific_calculator/KeyPad.java
@@ -0,0 +1,32 @@
+package nyc.c4q.rusili.scientific_calculator;
+
+import android.widget.Button;
+
+/**
+ * Created by RedDragon on 10/7/16.
+ */
+
+public class KeyPad {
+    /*Key Pad special functions*/
+    public Button ANS;
+    public Button DEL;
+    public Button CE;
+    public Button equals;
+
+    public Button division;
+    public Button multiply;
+    public Button subtract;
+    public Button addition;
+
+    /* Key Pad numbers */
+    public Button num9;
+    public Button num8;
+    public Button num7;
+    public Button num6;
+    public Button num5;
+    public Button num4;
+    public Button num3;
+    public Button num2;
+    public Button num1;
+    public Button num0;
+}
